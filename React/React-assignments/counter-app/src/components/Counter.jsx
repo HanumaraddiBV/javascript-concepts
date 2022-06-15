6739d922b0925c8d96017b3d7a12a5d3dc17ea8f
Hanumaraddi vaddatti <93374801+HanumaraddiBV@users.noreply.github.com>
2022-06-15 12:16:05 +0530

    added React components notes

--- React/React-assignments/counter-app/src/components/Counter.jsx
+++ React/React-assignments/counter-app/src/components/Counter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import "./counter.css"
+import "./counter.css";
 class Counter extends React.Component {
-  constructor(props){
-  super(props)
-   this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
       counter: 0,
-    }
+    };
   }
   increment = () => {
     this.setState({
@@ -22,17 +22,28 @@ class Counter extends React.Component {
   render() {
     return (
       <React.Fragment>
-      <div className="counter-container">
-      
-      
-      <div style={{border:"1px solid black", borderRadius:"50%",backgroundColor : this.state.counter == 0 ? "green" : this.state.counter > 0 ? "yellow":"red",width:"200px",height:"200px"}}></div>
-      <h2>{this.state.counter}</h2>
-      <button onClick={this.increment}>increment</button>
-      <button onClick={this.decrement}>decrement</button>
-      </div>
+        <div className="counter-container">
+          <div
+            style={{
+              border: "1px solid black",
+              borderRadius: "50%",
+              backgroundColor:
+                this.state.counter === 0
+                  ? "green"
+                  : this.state.counter > 0
+                  ? "yellow"
+                  : "red",
+              width: "200px",
+              height: "200px",
+            }}
+          ></div>
+          <h2>{this.state.counter}</h2>
+          <button onClick={this.increment}>increment</button>
+          <button onClick={this.decrement}>decrement</button>
+        </div>
       </React.Fragment>
     );
   }
 }
 
-export default Counter
+export default Counter;
